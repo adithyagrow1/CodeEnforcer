c2add72e3c39f7022984ee9c6e26ee57082885a1
Adithya <165917425+adithyagrow1@users.noreply.github.com>
2026-06-11 21:00:18 +0530

    CodeEnforcer

--- main.py
+++ main.py
@@ -4,12 +4,7 @@ from reviewer import review_chunk
 import json
 
 
-<<<<<<< Updated upstream
-=======
-#io
 
-
->>>>>>> Stashed changes
 def review_pr(pr_url):
     print(f"\nFetching PR: {pr_url}")
     pr_data = get_pr_diff(pr_url)
